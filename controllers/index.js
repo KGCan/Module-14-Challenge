592f1a7ed85cbcbfec3e145836c4f93682a72dca
PacDraft <Kirsten@PacificDraftWorks.com>
2022-03-19 18:13:17 -0700

    corrected connection errors in controllers

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
-const dashboardRoutes = require('./dashboard-routes');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
