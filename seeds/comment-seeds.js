d7e3c71f502cb36c86d88dc4fe9bb16509cbb95a
PacDraft <Kirsten@PacificDraftWorks.com>
2022-03-21 11:54:51 -0700

    added single entries for user, post and comment seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,6 +2,11 @@ const { Comment } = require("../models");
 
 const commentData= [
     // Add some comment info here to use as starter comments
+    {
+        user_id: 1,
+        post_id: 1,
+        comment_text: "I'm so excited to have a great place to communicate with like minded techies!"
+    },
 ]
 
 const starterComments = () => Comment.bulkCreate(commentData);
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,6 +2,12 @@ const { User } = require("../models");
 
 const userData = [
     //add user info here for seeded data
+    {
+        username: "KGCan",
+        github: "KGCan",
+        email: "kirsten@pacificdraftworks.com",
+        password: "12345",
+    },
 ]
 
 const starterUsers = () => User.bulkCreate(userData);
